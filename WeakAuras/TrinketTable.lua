09dd5179113b33136edba51c45f10023d5880fa7
userTim <usertim1205@gmail.com>
2021-02-07 07:29:51 +0900

    added trinket table

---
+++ WeakAuras/TrinketTable.lua
@@ -0,0 +1,58 @@
+WA_Proc_Item_Table = {
+    [40255] = { 
+        procName = 'Dying Curse',
+        stackable = false,
+        showGlow = true,
+    },
+    [50365] = { 
+        procName = 'Siphoned Power',
+        stackable = false,
+        showGlow = true,
+    },
+    [50360] = { 
+        procName = 'Siphoned Power',
+        stackable = false,
+        showGlow = true,
+    },
+    [50259] = { 
+        procName = 'Deadly Precision',
+        stackable = true,
+        showGlow = true,
+    },
+    [45518] = { 
+        procName = 'Flame of the Heavens',
+        stackable = false,
+        showGlow = true,
+    },
+    [47213] = {
+        procName = 'Elusive Power',
+        stackable = false,
+        showGlow = true,
+    },
+    [40432] = {
+        procName = 'Illustration of the Dragon Soul',
+        stackable = true
+    },
+    [50353] = {
+        procName = 'Surging Power',
+        stackable = true,
+        showGlow = true,
+        duration = 20
+    },
+    [50348] = {
+        procName = 'Surging Power',
+        stackable = true,
+        showGlow = true,
+        duration = 20
+    },
+    [40682] = {
+        procName = 'Now is the time!',
+        stackable = false,
+        showGlow = true        
+    },
+    [37660] = {
+        procName = 'Forge Ember',
+        stackable = false,
+        showGlow = true    
+    }
+}
